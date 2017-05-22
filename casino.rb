ec1ac65230e29167d45e5a6e203e56f2f6e10565
Ransom Valley <ransomwy@gmail.com>
2017-05-22 10:52:18 -0600

    Heads or Tails Game

--- casino.rb
+++ casino.rb
@@ -4,14 +4,28 @@ require 'colorize'
 
 #Pull in other classes you may need
 require_relative 'player'
+require_relative 'heads_or_tails'
 
 class Casino
   attr_accessor :player
+
   def initialize
     puts 'Welcome To Our Ruby Casino!'
     @player = Player.new
-    binding.pry
+    # need a casino menu method
+    casino_menu
   end
+
+  def casino_menu
+    #casino options
+    #game options
+    # bank-roll
+    #quit
+    HeadsTails.new(player)
+  end
+
+
 end
+
 
 Casino.new
