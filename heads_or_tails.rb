76eb8ce7ed0195f0516c656994b234c00f1d4745
Josie Thompson <josiecthompson@gmail.com>
2017-05-23 23:15:15 -0600

    heads tails and roulette

--- heads_or_tails.rb
+++ heads_or_tails.rb
@@ -2,24 +2,20 @@ class HeadsTails
   def initialize(player)
     puts "Welcome to Heads or Tails!"
     puts "#{player.name} you have a balance of: #{player.wallet.amount}"
-    #ask them how much they want to bet
     puts "How much do you want to bet?"
-      #I can make sure it's an integer
     bet_amount = gets.to_i
     dealer_heads_tails = rand(2)
     puts "Do you want heads or tails?"
     puts "Choose 0 for heads or 1 for tails"
-      #I want to make sure it's a 0 or 1
-      #if 0 elsif else
     user_heads_tails = gets.to_i
     if dealer_heads_tails == user_heads_tails 
-      puts "You win!" #add money
+      puts "You win!" 
     else
-      puts "You lose" #subtract money
+      puts "You lose"
     end
+    
     puts "Do you want to play again? yes / no"
     play_again = gets.strip.downcase
-    #case satement
     case play_again
     when "yes", "y"
       HeadsTails.new(player)
@@ -28,4 +24,3 @@ class HeadsTails
     end
   end
 end
-#decide how to build wallet how to add money or subtract money
--- roulette.rb
+++ roulette.rb
@@ -1,4 +1,3 @@
-# require 'pry'
 @roulette_array = [
   [1, "Red", "Odd"],
   [2, "Black", "Even"],
@@ -41,14 +40,15 @@ def roulette_bet
   puts "Welcome to Roulette!"
   puts "What do you want to bet on?"
   puts "1) Red/Black"
-  puts "2) Odd/Even"
-  puts "3) Numbers"
-  puts "4) Go back"
+  puts "2) Even/Odd"
   case gets.to_i
   when 1
     red_black
+  when 2
+    even_odd 
   end
 end
+
 def red_black
   puts "How much do you want to bet?"
   bet = gets.to_i
@@ -57,6 +57,7 @@ def red_black
   puts "2) Black"
   red_black_choice
 end
+
 def red_black_choice
   @choice = gets.strip
   if @choice == "1"
@@ -69,67 +70,55 @@ def red_black_choice
   end
   red_black_results
 end
+
 def red_black_results
   @number = @roulette_array.sample
   if @choice == @number[1]
     puts "You win!"
-    # winner
-    # add your bet to your wallet
   else
     puts "You lose!"
-    # lose 
-    # lose your bet
   end
-  # binding.pry
 end
+
 def generate_spin
   @number = @roulette_array.sample
 end
 roulette_bet
 
-# @roulette_array = [
-#   [1, "Red", "Odd"],
-#   [2, "Black", "Even"],
-#   [3, "Red", "Odd"],
-#   [4, "Black", "Even"],
-#   [5, "Red", "Odd"],
-#   [6, "Black", "Even"],
-#   [7, "Red", "Odd"],
-#   [8, "Black", "Even"],
-#   [9, "Red", "Odd"],
-#   [10, "Black", "Even"],
-#   [11, "Black", "Odd"],
-#   [12, "Red", "Even"],
-#   [13, "Black", "Odd"],
-#   [14, "Red", "Even"],
-#   [15, "Black", "Odd"],
-#   [16, "Red", "Even"],
-#   [17, "Black", "Odd"],
-#   [18, "Red", "Even"],
-#   [19, "Red", "Odd"],
-#   [20, "Black", "Even"],
-#   [21, "Red", "Odd"],
-#   [22, "Black", "Even"],
-#   [23, "Red", "Odd"],
-#   [24, "Black", "Even"],
-#   [25, "Red", "Odd"],
-#   [26, "Black", "Even"],
-#   [27, "Red", "Odd"],
-#   [28, "Black", "Even"],
-#   [29, "black", "Odd"],
-#   [30, "Red", "Even"],
-#   [31, "Black", "Odd"],
-#   [32, "Red", "Even"],
-#   [33, "Black", "Odd"],
-#   [34, "Red", "Even"],
-#   [35, "Black", "Odd"],
-#   [36, "Red", "Even"],
-# ]
+def even_odd
+  puts "How much do you want to bet?"
+  bet = gets.to_i
+  puts "Do you want to bet on even or odd?"
+  puts "1) Even"
+  puts "2) Odd"
+  even_odd_choice
+end
+
+def even_odd_choice
+  @choice = gets.strip
+  if @choice == "1"
+    @choice = "Even"
+  elsif @choice == "2"
+    @choice = "Odd"
+  else 
+    puts "Invalid input. Try again!"
+    even_odd_choice
+  end
+  even_odd_results
+end
+
+def even_odd_results
+  @number = @roulette_array.sample
+  if @choice == @number[2]
+    puts "You win!"
+  else
+    puts "You lose!"
+  end
+end
+
+def generate_spin
+  @number = @roulette_array.sample
+end
+roulette_bet
 
 
-# def roulette_bet()
-#   puts "Welcome to Roulette!"
-#   puts "What do you want to bet on?"
-#   end
-# Puts "Do you want to bet on red or black?"
- 
